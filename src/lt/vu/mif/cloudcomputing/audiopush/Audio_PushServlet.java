24a4ab2c56092ff4dc76681a7b66761f54bc58f4
Sarunas <sarunas.rimsa@gmail.com>
2013-09-13 21:26:28 +0300

    Trying git

---
+++ src/lt/vu/mif/cloudcomputing/audiopush/Audio_PushServlet.java
@@ -0,0 +1,13 @@
+package lt.vu.mif.cloudcomputing.audiopush;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Audio_PushServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello");
+	}
+}
